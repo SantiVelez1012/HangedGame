99a5bc089f89d9b2ca68b9d96efd9655efa7eab6
SantiVelez1012 <santivekez1012@gmail.com>
2021-07-05 19:05:28 -0500

    version 2.0

--- src/ahorcadogame/AhorcadoGame.java
+++ src/ahorcadogame/AhorcadoGame.java
@@ -17,71 +17,77 @@ public class AhorcadoGame {
         lectorPalabras objetoPalabras = new lectorPalabras();
         
         int dificultad = 0;
-        
-        
-        System.out.println("---------------------------------------------");
-        System.out.println("");
-        System.out.println("BIENVENIDO AL JUEGO DEL AHORCADO");
-        System.out.println("");
+        byte comprobacion = 0;
         
         do{
-            System.out.println("Ingrese la dificultad (1 - Facil -- 2 - Normal -- 3 - Dificil): ");
-            dificultad = teclado.nextInt();
-        }while(dificultad < 1 || dificultad > 3);
-        
-        ArrayList<String> listaPalabras = objetoPalabras.lecturaArchivo(dificultad);
-        
-        String palabraElegida = objetoPalabras.eleccionPalabra(listaPalabras);
-        
-        char[] caracteresPalabra = objetoPalabras.conversionStringCharArray(palabraElegida);
-        
-        mecanicauno objetoMecanica = new mecanicauno(caracteresPalabra);
-        
-        char[] palabraUsuario = objetoMecanica.creacionArrayGuiones();
-        
-        
-        int numeroIntentos = 0, numeroFallos = 0;
-        int fallosMaximos = 10;
-        char caracterUsuario;
-        
-        System.out.println("-----------------------------------------------");
-        System.out.println("COMIENZA EL JUEGO");
-        
-        
-        do{
-            
-            boolean victoria = objetoMecanica.comprobarVictoria(palabraUsuario);
-            
-            if(numeroFallos == fallosMaximos){
-                System.out.println(" HA PERDIDO ");
-                break;
-            }else if(victoria == true){
-                objetoMecanica.impresionPalabra(palabraUsuario);
-                System.out.println("Usted Ha ganado el juego");
-                break;
-            }
-            
+            System.out.println("---------------------------------------------");
             System.out.println("");
-            objetoMecanica.impresionPalabra(palabraUsuario);
+            System.out.println("BIENVENIDO AL JUEGO DEL AHORCADO");
+            System.out.println("");
+
+            do{
+                System.out.println("Ingrese la dificultad (1 - Facil -- 2 - Normal -- 3 - Dificil): ");
+                dificultad = teclado.nextInt();
+            }while(dificultad < 1 || dificultad > 3);
+
+            ArrayList<String> listaPalabras = objetoPalabras.lecturaArchivo(dificultad);
+
+            String palabraElegida = objetoPalabras.eleccionPalabra(listaPalabras);
+
+            char[] caracteresPalabra = objetoPalabras.conversionStringCharArray(palabraElegida);
+
+            mecanicauno objetoMecanica = new mecanicauno(caracteresPalabra);
+
+            char[] palabraUsuario = objetoMecanica.creacionArrayGuiones();
+
+
+            int numeroIntentos = 0, numeroFallos = 0;
+            int fallosMaximos = 10;
+            char caracterUsuario;
+
+            System.out.println("-----------------------------------------------");
+            System.out.println("COMIENZA EL JUEGO");
+
+
+            do{
+
+                boolean victoria = objetoMecanica.comprobarVictoria(palabraUsuario);
+
+                if(numeroFallos == fallosMaximos){
+                    System.out.println(" HA PERDIDO ");
+                    break;
+                }else if(victoria == true){
+                    objetoMecanica.impresionPalabra(palabraUsuario);
+                    System.out.println("");
+                    System.out.println("------------------------------------");
+                    System.out.println("Sus intentos totales fueron: " + numeroIntentos);
+                    System.out.println("Sus errores totales fueron: " + numeroFallos);
+                    System.out.println("FELICIDADES POR COMPLETAR EL JUEGO!");
+                    System.out.println("Usted Ha ganado el juego");
+                    System.out.println("");
+                    System.out.println("Desea jugar de nuevo? 1 - SI, 0 - NO");
+                    comprobacion = teclado.nextByte();
+                    break;
+                }
+
+                System.out.println("");
+                objetoMecanica.impresionPalabra(palabraUsuario);
+
+                System.out.println("Ingrese una letra: ");
+                caracterUsuario = teclado.next().charAt(0);
+
+                boolean existe = objetoMecanica.recibirCaracter(palabraUsuario, caracterUsuario);
+
+                if(existe == false){
+                    System.out.println("Ha fallado, sigue intentando");
+                    numeroFallos++;
+                }
+
+                numeroIntentos++;
             
-            System.out.println("Ingrese una letra: ");
-            caracterUsuario = teclado.next().charAt(0);
-            
-            boolean existe = objetoMecanica.recibirCaracter(palabraUsuario, caracterUsuario);
-            
-            if(existe == false){
-                System.out.println("Ha fallado, sigue intentando");
-                numeroFallos++;
-            }
-            
-            numeroIntentos++;
-            
-        }while(palabraUsuario != caracteresPalabra);
+            }while(palabraUsuario != caracteresPalabra);
+        }while (comprobacion != 0);
         
-        
-        if(palabraUsuario == caracteresPalabra){
-            System.out.println("FELICIDADES POR COMPLETAR EL JUEGO!");
-        }
         
         
         
